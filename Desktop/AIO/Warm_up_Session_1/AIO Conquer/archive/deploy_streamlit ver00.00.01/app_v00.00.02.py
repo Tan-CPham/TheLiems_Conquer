728853c5065c76bc99a344aa57b6473a67e18619
Ly Duc Hoang <91303304+User1918@users.noreply.github.com>
2026-01-31 22:19:47 +0700

    Change model paths to relative in app_v00.00.02.py
    Updated model paths to relative paths for deployment.

--- Desktop/AIO/Warm_up_Session_1/AIO Conquer/archive/deploy_streamlit ver00.00.01/app_v00.00.02.py
+++ Desktop/AIO/Warm_up_Session_1/AIO Conquer/archive/deploy_streamlit ver00.00.01/app_v00.00.02.py
@@ -8,10 +8,10 @@ import sklearn
 
 
 # Model paths
-RF_MODEL_PATH = r"C:\Users\ACER\Desktop\AIO\Warm_up_Session_1\AIO Conquer\archive\deploy_streamlit ver00.00.01\random_forest_model.joblib"
-LR_MODEL_PATH = r"C:\Users\ACER\Desktop\AIO\Warm_up_Session_1\AIO Conquer\archive\deploy_streamlit ver00.00.01\logistic_regression_model.joblib"
-SCALER_PATH = r"C:\Users\ACER\Desktop\AIO\Warm_up_Session_1\AIO Conquer\archive\deploy_streamlit ver00.00.01\scaler.pkl"
-FEATURES_PATH = r"C:\Users\ACER\Desktop\AIO\Warm_up_Session_1\AIO Conquer\archive\deploy_streamlit ver00.00.01\model_features.pkl"
+RF_MODEL_PATH = r"\deploy_streamlit ver00.00.01\random_forest_model.joblib"
+LR_MODEL_PATH = r"\deploy_streamlit ver00.00.01\logistic_regression_model.joblib"
+SCALER_PATH = r"\deploy_streamlit ver00.00.01\scaler.pkl"
+FEATURES_PATH = r"\deploy_streamlit ver00.00.01\model_features.pkl"
 
 @st.cache_resource
 def load_artifacts():
